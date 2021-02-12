606f3c9e864595f374587abfb1dde37bdaaa6aab
Tolga Alper Kuş <tolgaalperkus@hotmail.com>
2021-02-12 03:12:50 +0300

    Blog

---
+++ BlogApp/BlogApp.WebUI/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApp.Data.Abstract;
+using BlogApp.Data.Concrete.EfCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogApp.WebUI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ICategoryRepository, EfCategoryRepository>();
+            services.AddTransient<IBlogRepository,EfBlogRepository>();
+            services.AddDbContext<BlogContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),b=>b.MigrationsAssembly("BlogApp.WebUI"))); 
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();//Hata sayfalarini gostermesi icin
+            }
+            app.UseStatusCodePages();//Durum sayfalari gozuksun 
+            app.UseStaticFiles(); //wwwroot icerisindeki statik dosyalara erisim
+            //mvcyi varsayilan route ayariyla kullanmaya basliyoruz
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            SeedData.Seed(app);
+        }
+    }
+}
